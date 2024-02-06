3a65084be9aa6b9c00a7f5e4b86e04d36d4356ea
David Diaz <david.daniel.diaz@gmail.com>
2024-02-06 18:19:43 -0500

    Runs of FIA across USA with FVS autocalibration

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='Calibrating FVS using keywords and FIA data',
-    author='Ecotrust',
-    license='BSD-3',
+    version="0.1.0",
+    description="Calibrating FVS using keywords and FIA data",
+    author="Ecotrust",
+    license="BSD-3",
 )
